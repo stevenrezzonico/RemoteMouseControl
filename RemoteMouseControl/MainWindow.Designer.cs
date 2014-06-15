d19dca7565cdfbbca799b35f6fa670c27a907eca
stevenrezzonico <steven.rezzonico@gmail.com>
2014-06-15 22:44:17 +0200

    Initial Release

---
+++ RemoteMouseControl/MainWindow.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RemoteMouseControl {
+    partial class MainWindow {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.IpLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // IpLabel
+            // 
+            this.IpLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.IpLabel.AutoSize = true;
+            this.IpLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IpLabel.Location = new System.Drawing.Point(9, 115);
+            this.IpLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.IpLabel.Name = "IpLabel";
+            this.IpLabel.Size = new System.Drawing.Size(147, 31);
+            this.IpLabel.TabIndex = 0;
+            this.IpLabel.Text = "IP Address";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 269);
+            this.Controls.Add(this.IpLabel);
+            this.Name = "MainWindow";
+            this.Text = "Remote Mouse Control";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label IpLabel;
+    }
+}
+
---
+++ RemoteMouseControl/MainWindow.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteMouseControl {
+    public partial class MainWindow : Form {
+        private Task listenerTask;
+
+        //DLL used to set cursor´s position
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        public MainWindow() {
+            InitializeComponent();
+
+            //Receiver backgroud task
+            listenerTask = Task.Factory.StartNew(ListenMessages, TaskCreationOptions.LongRunning);
+            listenerTask.ContinueWith(t => { }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            //Print machine IP
+            IpLabel.Text = GetDefaultIP();
+        }
+
+        private static string GetDefaultIP() {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+
+            var gateway = (adapters.Where(nic => nic.OperationalStatus == OperationalStatus.Up)
+                .Select(nic => nic.GetIPProperties().GatewayAddresses.First())
+                ).FirstOrDefault().Address;
+
+            foreach (NetworkInterface adapter in adapters) {
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+
+                if (properties.GatewayAddresses.Count <= 0) continue;
+                if (adapter.OperationalStatus == OperationalStatus.Up &&
+                    properties.GatewayAddresses.First().Address == gateway) {
+                    foreach (var x in properties.UnicastAddresses.Where(x => x.Address.AddressFamily == AddressFamily.InterNetwork)) {
+                        return x.Address.ToString();
+                    }
+                }
+            }
+
+            return "Null IP";
+        }
+
+        private void ListenMessages() {
+            while (true) {
+                var sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                var iep = new IPEndPoint(IPAddress.Any, Properties.Settings.Default.Port);
+                sock.Bind(iep);
+                var ep = (EndPoint)iep;
+
+                var data = new byte[32];
+                int recv = sock.ReceiveFrom(data, ref ep);
+
+                string stringData = Encoding.UTF8.GetString(data, 0, recv);
+                MessageBox.Show(stringData);
+
+                string[] arrayData = stringData.Split(';');
+
+
+                int x = int.Parse(arrayData[0]);
+                int y = int.Parse(arrayData[1]);
+
+                //MessageBox.Show("X:" + x + " Y:" + y);
+
+                //MessageBox.Show("X:" + MousePosition.X + " Y:" + MousePosition.Y);
+
+                int newX = MousePosition.X + MousePosition.X * x / 100;
+                int newY = MousePosition.Y + MousePosition.Y * y / 100;
+
+                //MessageBox.Show("X:" + newX + " Y:" + newY);
+
+                //xLabel.Text = "Delta X: " + x;
+                //yLabel.Text = "Delta Y: " + y;
+
+                BeginInvoke(new Action(() => SetCursorPos(newX, newY)));
+
+                sock.Close();
+            }
+        }
+    }
+}
